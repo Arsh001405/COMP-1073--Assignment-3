6082bc2ea060cafb326473dda7f4c27a26139cec
Arsh001405 <arshkahlon.14.2005@gmail.com>
2025-08-15 09:45:04 -0400

    added url to link from api site to get the data for the site

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ document.getElementById("searchBtn").addEventListener("click", () => {
     }
     getWeather(city);
 });
+
+function getWeather(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById("cityName").textContent = data.name;
+            document.getElementById("description").textContent = data.weather[0].description;
+            document.getElementById("temperature").textContent = data.main.temp;
+            document.getElementById("humidity").textContent = data.main.humidity;
+            document.getElementById("windSpeed").textContent = data.wind.speed;
+            document.getElementById("feelsLike").textContent = data.main.feels_like;
+
+            const iconCode = data.weather[0].icon;
+            document.getElementById("weatherIcon").src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+            document.getElementById("weatherResult").classList.remove("hidden");
+        })
+        .catch(error => {
+            alert(error.message);
+        });
+}
